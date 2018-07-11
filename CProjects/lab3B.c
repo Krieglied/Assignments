1b9a24e31634fd538905648a6303ff28a6254ade
student <student@localhost.localdomain>
2018-07-11 10:55:23 -0500

    Lab 3B Added

---
+++ CProjects/lab3B.c
@@ -0,0 +1,68 @@
+/*
+ *
+ * Lab 3B for Character String Array
+ *
+ * Name: Robert John Graham III
+ * Date: July 11, 2018
+ * Project: Character String
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char quote[256] = {0};
+	quote[0] = 'I';
+	quote[1] = '\'';
+	quote[2] = 'v';
+	quote[3] = 'e';
+	quote[4] = '\n';
+	quote[5] = 'h';
+	quote[6] = 'u';
+	quote[7] = 'r';
+	quote[8] = 't';
+	quote[9] = '\n';
+	quote[10] = 'y';
+	quote[11] = 'o';
+	quote[12] = 'u';
+	quote[13] = ',';
+	quote[14] = '\n';
+	quote[15] = 'a';
+	quote[16] = 'n';
+	quote[17] = 'd';
+	quote[18] = '\n';
+	quote[19] = 'I';
+	quote[20] = '\n';
+	quote[21] = 'w';
+	quote[22] = 'i';
+	quote[23] = 's';
+	quote[24] = 'h';
+	quote[25] = '\n';
+	quote[26] = 't';
+	quote[27] = 'o';
+	quote[28] = '\n';
+	quote[29] = 'g';
+	quote[30] = 'o';
+	quote[31] = '\n';
+	quote[32] = 'o';
+	quote[33] = 'n';
+	quote[34] = '\n';
+	quote[35] = 'h';
+	quote[36] = 'u';
+	quote[37] = 'r';
+	quote[38] = 't';
+	quote[39] = 'i';
+	quote[40] = 'n';
+	quote[41] = 'g';
+	quote[42] = '\n';
+	quote[43] = 'y';
+	quote[44] = 'o';
+	quote[45] = 'u';
+	quote[46] = '.';
+	quote[47] = '\n';
+	quote[48] = '\0';
+	printf("%s",quote);
+	
+	return 0;
+}
