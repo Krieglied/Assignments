5f549bc3385004ed99becdb436f0c9ac3958578f
Krieglied <kriegliedthosshys@gmail.com>
2018-07-13 09:18:03 -0500

    Demo Pythagoras added.

---
+++ CProjects/demoPy.c
@@ -0,0 +1,27 @@
+/* Demo Lab
+ * July 13,2018
+ * Robert John Graham III
+ *
+ *
+ * /
+
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	double legA = 0;
+	double legB = 0;
+	double hypotenuse = 0;
+
+	printf("Input the length of leg A and leg B for your right triangle. \n");
+	printf("Separate the two lengths with a tab.  (e.g., 1.3 9.7) \n");
+	scanf("%lf\t%lf", &legA, &legB);
+
+	hypotenuse = sqrt((legA*legA) + (legB*legB));
+	printf("Your hypotenuse is %.2f. \n", hypotenuse);
+
+	getchar();
+	return 0;
+}
