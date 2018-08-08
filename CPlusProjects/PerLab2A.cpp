4005bffca60267b8603ea491639a1995eede2f2f
Krieglied <41011777+Krieglied@users.noreply.github.com>
2018-08-08 14:49:46 -0500

    Update PerLab2A.cpp

--- CPlusProjects/PerLab2A.cpp
+++ CPlusProjects/PerLab2A.cpp
@@ -19,63 +19,66 @@
 #include <fstream>
 #include <list>
 
-void print_queue(const std::list<std::string>& queue);
 int count_word(std::list<std::string>::iterator& it);
 
 int main()
 {
+	//Input text file
 	std::ifstream file("lyrics.txt");
 	std::string line;
 	std::list<std::string> contents;
 	if(file.is_open())
 	{
+		//The file is looked line by line
 		while (std::getline(file, line))
-        	{
+        {
 			std::string word;
-            		for(const auto& item : line)
+			//Each line is going to consist of words, so if character by character is looked at
+            for(const auto& item : line)
 			{
-				//std::cout << item;
+				//If the character is not blank, then add the character to the current word
 				if(item != ' ')
 				{
 					word += item;
 				}
+				//If it is blank, then the word is added to the list, and word is reset
 				else
 				{
 					contents.push_back(word);
 					word = "";
 				}
 			}
+			//If the line is at the end, then the last word needs to added to the list
 			contents.push_back(word);
-        	}
-        	file.close();
+        }
+        file.close();
 	}
+	//List is sorted
 	contents.sort();
+	//This iterator will be sent to the function
 	auto con_iterator = contents.begin();
+	//This loop needs to be changed to translate list to map with <int, string>
 	while(con_iterator != contents.end())
 	{
 		std::cout << "| " << *con_iterator << " | ";
 		std::cout << count_word(con_iterator) << " |" << std::endl;;
 	}
-	//print_queue(contents);
 	return 0;
-}
-void print_queue(const std::list<std::string>& queue)
-{
-	for(const auto& item : queue)
-        {
-                std::cout << item << std::endl;
-        }
 }
 int count_word(std::list<std::string>::iterator& it)
 {
+	//Start at 1, because by virtue of passing the iterator, there is already at least 1
 	int count = 1;
 	bool match = true;
 	while(match)
 	{
+		//If next slot matches, then count needs to be increased
+		//And this line will increase the iterator
 		if(*it == *(it++))
 		{
 			count++;
 		}
+		//Otherwise, break loop to return count
 		else
 		{
 			match = false;
