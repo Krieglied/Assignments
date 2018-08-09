9fd624f56139f1b4ea2be87a28b8cd39b5337578
Krieglied <41011777+Krieglied@users.noreply.github.com>
2018-08-08 20:36:42 -0500

    Update PerLab2A.cpp

--- CPlusProjects/PerLab2A.cpp
+++ CPlusProjects/PerLab2A.cpp
@@ -17,46 +17,41 @@
 
 #include <iostream>
 #include <fstream>
-#include <list>
-a//Performance Lab 2A
-//Robert John Graham III
-//August 8, 2018
-//
-//
-//Lab 2A
-//
-//---
-//
-//Provided a text file, use the STL to read from it and count each occurrence of each word. Once complete, output to **stdout **each word, and the number of times it occurred. //Sort the output by words that occured the most to words that occured the least.
-//
-//You will write this program in its entirety. There will be no source code provided by the instructor, only a text file containing the input.
-//
-//The instructional goal of this lab is to give you practice using the STL. Use _STL containers_, _algorithms_, and _iterators_ where they would be helpful.
-
-//Provided text file is lyrics.txt
-
-#include <iostream>
-#include <fstream>
 #include <string>
-#include <list>
 #include <map>
+#include <set>
 
-int count_word(std::list<std::string>::iterator& it, const std::list<std::string>::iterator& end);
+void find_word(const std::string& input, std::map< std::string, int>& map);
+
+//This is a comparison section for the second element (the value of the map)
+struct comp
+{
+	template<typename T>
+	bool operator()(const T& l, const T& r) const
+	{
+		//The count (value portion of the element) is sort higher value to lower values
+		if (l.second != r.second)
+		{
+			return l.second > r.second;
+		}
+		//If the count is equal between the two elements, then the elements are sorted by the string (key portion of the element)
+		return l.first > r.first;
+	}
+};
 
 int main()
 {
 	//Input text file
 	std::ifstream file("lyrics.txt");
 	std::string line;
-	std::list<std::string> contents;
 	std::map< std::string, int> output;
 	if (file.is_open())
 	{
-		//The file is looked line by line
+		//The file is looked at,line by line
 		while (std::getline(file, line))
 		{
 			std::string word;
-			//Each line is going to consist of words, so if character by character is looked at
+			//Each line is going to consist of words, so character by character is looked at
 			for (const auto& item : line)
 			{
 				//If the character is not blank, then add the character to the current word
@@ -64,124 +59,38 @@ int main()
 				{
 					word += item;
 				}
-				//If it is blank, then the word is added to the list, and word is reset
+				//If it is blank, then the word is added to the map and the word is reset
 				else
 				{
-					contents.push_back(word);
+					find_word(word, output);
 					word = "";
 				}
 			}
-			//If the line is at the end, then the last word needs to added to the list
-			contents.push_back(word);
+			//End of line has been reached, the last word waiting needs to be added to the map
+			find_word(word, output);
 		}
-		file.close();
 	}
-	//List is sorted
-	contents.sort();
-	//This iterator will be sent to the function
-	auto con_iterator = contents.begin();
-	//This loop needs to be changed to translate list to map with <int, string>
-	while (con_iterator != contents.end())
+	//The map is moved to set, sorting it by the value and then the key, higher values to lower values
+	std::set<std::pair<std::string, int>, comp> set(output.begin(), output.end());
+	for (const auto& item : set)
 	{
-		std::string word = *con_iterator;
-		output.emplace(word, count_word(con_iterator, contents.end()));
-		//std::cout << "| " << *con_iterator << " | ";
-		//std::cout << count_word(con_iterator) << " |" << std::endl;;
+		std::cout << "| " <<item.first << " | " << item.second << " |" <<std::endl;
 	}
-	while (1);
+	while (true);
 	return 0;
 }
-int count_word(std::list<std::string>::iterator& it, const std::list<std::string>::iterator& end)
+void find_word(const std::string& input, std::map< std::string, int>& map)
 {
-	//Start at 1, because by virtue of passing the iterator, there is already at least 1
-	int count = 1;
-	bool match = true;
-	std::list<std::string>::iterator& test = it;
-	while (match)
+	//Here the map is searched to see if the key has already been input
+	auto search = map.find(input);
+	//If the word has been found(i.e. iterator is not equal to the end iterator), then the value++
+	if (search != map.end())
 	{
-		if (test++ == end)
-		{
-			break;
-		}
-		//If next slot matches, then count needs to be increased
-		//And this line will increase the iterator
-		if (*it == *(test++))
-		{
-			count++;
-		}
-		//Otherwise, break loop to return count
-		else
-		{
-			match = false;
-		}
-		it++;
+		search->second++;
 	}
-	return count;
-}
-int count_word(std::list<std::string>::iterator& it);
-
-int main()
-{
-	//Input text file
-	std::ifstream file("lyrics.txt");
-	std::string line;
-	std::list<std::string> contents;
-	if(file.is_open())
+	//Otherwise, the word needs to be added to the map (with a value of 1, of course)
+	else
 	{
-		//The file is looked line by line
-		while (std::getline(file, line))
-        {
-			std::string word;
-			//Each line is going to consist of words, so if character by character is looked at
-            for(const auto& item : line)
-			{
-				//If the character is not blank, then add the character to the current word
-				if(item != ' ')
-				{
-					word += item;
-				}
-				//If it is blank, then the word is added to the list, and word is reset
-				else
-				{
-					contents.push_back(word);
-					word = "";
-				}
-			}
-			//If the line is at the end, then the last word needs to added to the list
-			contents.push_back(word);
-        }
-        file.close();
+		map.emplace(input, 1);
 	}
-	//List is sorted
-	contents.sort();
-	//This iterator will be sent to the function
-	auto con_iterator = contents.begin();
-	//This loop needs to be changed to translate list to map with <int, string>
-	while(con_iterator != contents.end())
-	{
-		std::cout << "| " << *con_iterator << " | ";
-		std::cout << count_word(con_iterator) << " |" << std::endl;;
-	}
-	return 0;
-}
-int count_word(std::list<std::string>::iterator& it)
-{
-	//Start at 1, because by virtue of passing the iterator, there is already at least 1
-	int count = 1;
-	bool match = true;
-	while(match)
-	{
-		//If next slot matches, then count needs to be increased
-		//And this line will increase the iterator
-		if(*it == *(it++))
-		{
-			count++;
-		}
-		//Otherwise, break loop to return count
-		else
-		{
-			match = false;
-		}
-	}
-	return count;
 }
