50dd82ef6fb787bcdc085fa31dfa54e3327b6b39
Krieglied <41011777+Krieglied@users.noreply.github.com>
2018-08-09 14:44:50 -0500

    Create Encode.cpp

---
+++ CPlusProjects/Encode.cpp
@@ -0,0 +1,35 @@
+//Bonus Lab Encode
+//Robert John Graham III
+//August 9, 2018
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+        //Input text file
+        std::ifstream file("textInput.txt");
+        std::string line;
+        std::string input;
+        if (file.is_open())
+        {
+                //The file is looked at,line by line
+                while (std::getline(file, line))
+                {
+                        for(const auto& item : line)
+                        {
+                                input += (char)(item + 3);
+                        }
+                        input += '\n';
+                }
+        }
+        file.close();
+        std::ofstream encodedfile("encodedText.txt");
+        if(encodedfile.is_open())
+        {
+                encodedfile << input;
+                encodedfile.close();
+        }
+        return 0;
+}
