ce12194730bf8c3b89182bbf5a7517e133d05db2
Krieglied <41011777+Krieglied@users.noreply.github.com>
2018-08-09 14:51:26 -0500

    Update Fancy_Hanoi.cpp

--- CPlusProjects/Fancy_Hanoi.cpp
+++ CPlusProjects/Fancy_Hanoi.cpp
@@ -14,7 +14,7 @@ int Setup(struct Tower& tower, int discs);
 void Hanoi(struct Tower& source, struct Tower& dest, struct Tower& temp, int n);
 void display(struct Tower& tower);
 void display_towers(struct Tower& source, struct Tower& dest, struct Tower& temp);
-static unsigned long long numMoves = 0;
+static uint128_t numMoves = 0;
 
 struct Tower
 {
