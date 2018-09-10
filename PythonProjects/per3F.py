9a0de7b84d65575a9351f8d42818a58a250cc900
Krieglied <kriegliedthosshys@gmail.com>
2018-09-10 09:15:18 -0500

    FizzBuzz v2 added

--- PythonProjects/per3F.py
+++ PythonProjects/per3F.py
@@ -12,15 +12,5 @@ counter = 100
 output = ""
 #Numbers from 1 to 100 are evaluated
 for i in range(1, counter + 1):
-    #If the number is divisable by 3 or 5, then output will just be the number
-    if i % 3 != 0 and i % 5 != 0:
-        output = i
-    #If the number is divisable by 3, output has Fizz at the beginning
-    if i % 3 == 0:
-        output = "Fizz"
-    #If the number is divisable by 5, add Buzz to the output
-    if i % 5 == 0:
-        output += "Buzz"
-    #Output is printed and then reset for the next loop
-    print output
-    output = ""
+    #print 'Fizz' * (i % 3 == 0) + 'Buzz' * (i % 5 == 0) or i
+    print i if (i % 3 != 0 and i % 5 != 0) else ("FizzBuzz" if (i % 3 == 0 and i % 5 == 0) else ("Fizz" if (i % 3 == 0) else "Buzz"))
