7ba6b66779f8e5142025e237fc38411fa14847f3
Krieglied <41011777+Krieglied@users.noreply.github.com>
2019-12-19 22:08:10 -0600

    Update genTestPrime.py

--- KringleCon2019/genTestPrime.py
+++ KringleCon2019/genTestPrime.py
@@ -2,11 +2,11 @@ import sympy
 
 primes = list(sympy.primerange(1137,7731))
 for prime in primes:
-    notprime = ['0','2','4','5','6','8','9']
+    notprime = ['a','b','c','d','e','f','g']
     found = True
     for nprime in notprime:
         if nprime in str(prime):
            found = False
     if found:
-        if not (str(prime).count('1') > 2 or str(prime).count('3') > 2 or str(prime).count('7') > 2):
+        if not (str(prime).count('a') > 2 or str(prime).count('b') > 2 or str(prime).count('c') > 2):
             print(prime)
